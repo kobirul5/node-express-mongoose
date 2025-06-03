570a4673b42fe52f713793be4f015af1d91a1929
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-06-03 23:14:39 +0600

    create route and  send sipmle resposne

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,9 +1,24 @@
 const http = require("http");
 
 const server = http.createServer((req,res)=>{
-    res.end("Welcome to ToDo App Server")
+    // console.log(req.url ,"------", req.method)
+    // res.end("Welcome to ToDo App Server")
+
+    if(req.url === "/todos" && req.method === "GET"){
+        res.end("All Todos")
+    }else if(req.url === "/todos/create-todo" && req.method === "POST"){
+        res.end("ToDo created")
+    }
+
 });
 
 server.listen(5000, "127.0.0.1", ()=>{
     console.log("server listing to port 5000")
 })
+
+
+
+/**
+ * /todos - GET All tods
+ * /todos/create-todos- POST Create Todo
+ */
