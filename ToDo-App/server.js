4b50dedc05bab60968f1f6f79e00c611f5ae4798
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-06-04 01:26:31 +0600

    get single data

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -6,41 +6,60 @@ const filePath = path.join(__dirname, "./db/todo.json")
 
 const server = http.createServer((req, res) => {
 
+  const url = new URL(req.url, `http://${req.headers.host}`)
+  const pathname = url.pathname
 
   // GET Todos
-  if (req.url === "/todos" && req.method === "GET") {
+  if (pathname === "/todos" && req.method === "GET") {
 
     const data = fs.readFileSync(filePath, { encoding: "utf8" })
     res.writeHead(200, {
       "content-type": "application/json",
     })
     res.end(data)
-  } 
+  }
 
   // Post ToDos  now
-
-  
-  else if (req.url === "/todos/create-todo" && req.method === "POST") {
+  else if (pathname === "/todos/create-todo" && req.method === "POST") {
     let data = ""
 
-    req.on("data", (chunk)=>{
+    req.on("data", (chunk) => {
       data = data + chunk
     })
 
-    req.on("end", ()=>{
+    req.on("end", () => {
       console.log(data)
-      const {title, body} = JSON.parse(data)
+      const { title, body } = JSON.parse(data)
       console.log(title, body)
       const createdAt = new Date().toLocaleString()
-      const allTodos = fs.readFileSync(filePath, {encoding:"utf-8"})
+      const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" })
 
       const parseAllTodos = JSON.parse(allTodos)
 
-      parseAllTodos.push({title, body, createdAt})
-      fs.writeFileSync(filePath, JSON.stringify(parseAllTodos), {encoding: "utf-8"})
-      res.end(JSON.stringify({title, body, createdAt}))
+      parseAllTodos.push({ title, body, createdAt })
+      fs.writeFileSync(filePath, JSON.stringify(parseAllTodos, null, 2), { encoding: "utf-8" })
+      res.end(JSON.stringify({ title, body, createdAt }, null, 2))
     })
   }
+
+  // Get single Data
+  else if (pathname === "/todo" && req.method === "GET") {
+
+    const title = url.searchParams.get("title")
+    const data = fs.readFileSync(filePath, { encoding: "utf-8" })
+
+    const parseData = JSON.parse(data)
+    const todo = parseData.find((todo)=>todo.title === title);
+
+    res.writeHead(200, {
+      "content-type": "application/json"
+    })
+
+    res.end(JSON.stringify(todo))
+  }
+
+  // Delete Data
+
 
 });
 
